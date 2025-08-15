10ad38b06e2541aa890b5bf7ac0096d451d14a16
tarxvrf <tarxvrf@gmail.com>
2025-08-15 21:51:39 +0700

    penambahan marque

--- src/app/Partner.tsx
+++ src/app/Partner.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React from "react";
-
+import Marquee from "react-fast-marquee";
 function Partner() {
   return (
     <div>
       {" "}
       {/* Partners Section */}
-      <section id="partners" className="pb-32 pt-16 bg-gray-50">
+      <section id="partners" className="pb-16 pt-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-6">
           <div className="text-center mb-10">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
@@ -19,66 +19,68 @@ function Partner() {
             </p>
           </div>
 
-          <div className="pt-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 items-center">
-            <div className="flex justify-center ">
-              <Image
-                width={100}
-                height={100}
-                src="/images/alfresh.jpeg"
-                alt="Client 1"
-                className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl  transform hover:-translate-y-2 transition"
-              />
-            </div>
+          <div className="">
+            <Marquee speed={20} gradient={false} pauseOnHover className="py-4">
+              <div className="flex justify-center ">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/alfresh.jpeg"
+                  alt="Client 1"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl  transform hover:-translate-y-2 transition"
+                />
+              </div>
 
-            <div className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src="/images/m165.jpeg"
-                alt="Client 2"
-                className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
-              />
-            </div>
+              <div className="flex justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/m165.jpeg"
+                  alt="Client 2"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2  object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
+                />
+              </div>
 
-            <div className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src="/images/boxis.jpeg"
-                alt="Client 3"
-                 className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
-            />
-            </div>
+              <div className="flex justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/boxis.jpeg"
+                  alt="Client 3"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2  object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
+                />
+              </div>
 
-            <div className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src="/images/detos.jpg"
-                alt="Client 4"
-                 className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
-            />
-            </div>
+              <div className="flex justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/detos.jpg"
+                  alt="Client 4"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2  object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
+                />
+              </div>
 
-            <div className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src="/images/livingalam.png"
-                alt="Client 5"
-                  className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
-            />
-            </div>
+              <div className="flex justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/livingalam.png"
+                  alt="Client 5"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2  object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
+                />
+              </div>
 
-            <div className="flex justify-center">
-              <Image
-                width={100}
-                height={100}
-                src="/images/bekasi.webp"
-                alt="Client 6"
-                className="h-32 w-32 object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
-            />
-            </div>
+              <div className="flex justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/bekasi.webp"
+                  alt="Client 6"
+                  className="w-16 h-16 sm:w-32 sm:h-32 mx-2  object-contain border bg-white rounded-full shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition"
+                />
+              </div>
+            </Marquee>
           </div>
         </div>
       </section>
