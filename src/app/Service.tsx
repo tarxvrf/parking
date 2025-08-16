22f60853e9eeb128d4598dd2407a8b571c3aa476
tarxvrf <tarxvrf@gmail.com>
2025-08-16 19:00:53 +0700

    bug service

--- src/app/Service.tsx
+++ src/app/Service.tsx
@@ -71,27 +71,7 @@ function Service() {
         </div>
       </section>
 
-      <div className="mx-auto flex justify-center py-16">
-        <Carousel className="w-full max-w-xs">
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">
-                        {index + 1}
-                      </span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+   
     </div>
   );
 }
